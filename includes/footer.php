fd238ebbb07074235181fbb2a38910fc897dca87
richardhapb <richard.penab@gmail.com>
2023-09-03 16:02:03 -0300

    Reestructuración de footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
     <div class="container">
         <div class="contact">
+            <h2 class="title"><span>Cont</span>acto</h2>
             <div class="person">
                 <ul>
                     <li><span>Laura Guerrero</span></li>
@@ -15,9 +16,11 @@
                     <li><span>Email:</span> <a href="mailto:richard.pena@menteingenieria.com">richard.pena@menteingenieria.com</a></li>
                 </ul>
             </div>
-            <div class="icons">
-                <h2>LinkedIn</h2>
-                <a href="https://www.linkedin.com/company/menteingenieria">
+        </div>
+        <div class="icons">
+            <h2 class="title"><span>Link</span>edIn</h2>
+            <a class="network" href="https://www.linkedin.com/company/menteingenieria">
+                <div class="icon">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
@@ -26,8 +29,9 @@
                     <path d="M12 16l0 -5" />
                     <path d="M16 16v-3a2 2 0 0 0 -4 0" />
                     </svg>
-                </a>
-            </div>
+                </div>
+                <p>menteingenieria</p>
+            </a>
         </div>
     </div>
 </footer>
