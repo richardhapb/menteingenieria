02be977c9e0c8cb1bc22e2140ae5194f82baca53
richardhapb <richard.penab@gmail.com>
2023-09-05 19:51:41 -0300

    Se incluye listado de servicios en brochure

--- brochure.php
+++ brochure.php
@@ -8,7 +8,23 @@
 <?php include("includes/header.php") ?>    
 <main class="container">
     <h1>Conócenos</h1>
-    <p> En mente ingeniería nos interesa mantener una relación de confianza con nuestros clientes, a continuación puedes obtener el catálogo de los servicios que ofrecemos y también algo de información adicional referente a nosotros.</p>
+    <p> En mente ingeniería nos interesa mantener una relación de confianza con nuestros clientes, en este apartado puedes obtener el catálogo de los servicios que ofrecemos y también algo de información adicional referente a nosotros.</p>
+
+    <p>Dentro de nuestro alcance se encuentran desarrollos con el manejo de información, gestión etratégica, ingeniería de procesos, automatización digital y soportes informáticos, a continuación detallamos algunos de ellos:</p>
+
+    <ul>
+        <li>Diseño, limpieza, estructura y normalización de bases de datos.</li>
+        <li>Creación, mantenimiento y consultas en bases de datos relacionales SQL.</li>
+        <li>Reportabilidad y análisis de datos con Python.</li>
+        <li>Scripts en Python.</li>
+        <li>Macros y programación VBA en Excel.</li>
+        <li>Creación de reportes dinámicos Power BI.</li>
+        <li>Presetaciones de estrategia o datos organizacionales.</li>
+        <li>Diagramas de procesos.</li>
+        <li>Investigación de mercado y tendencias.</li>
+        <li>Clases de Excel, básico, intermedio y avanzado.</li>
+        <li>Clases de Power BI, básico, intermedio y avanzado.</li>
+    </ul>
 
     <h2>Brochure</h2>
 
