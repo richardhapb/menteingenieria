3feb8d5f1aeafa106f3514cde90ecf61c65d4f7f
richardhapb <richard.penab@gmail.com>
2024-11-26 21:53:25 -0300

    feat: extra protection admin url and middleware

--- backend/menteingenieria/settings.py
+++ backend/menteingenieria/settings.py
@@ -4,98 +4,98 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'contacto',
-    'rest_framework',
-    'blog',
-    'storages',
-    'openai_requests',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "contacto",
+    "rest_framework",
+    "blog",
+    "storages",
+    "openai_requests",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'openai_requests.middleware.RestrictIPMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "openai_requests.middleware.RestrictIPMiddleware",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE = "Lax"
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173',
-    'http://localhost:4173',
-    'http://127.0.0.1',
-    'https://3.145.58.151',
-    'https://3.145.58.151:443',
-    'http://3.145.58.151:443',
-    'http://179.2.28.24',
-    'https://179.2.28.24',
-    'http://172.26.13.221',
-    'https://172.26.13.221',
-    'https://www.menteingenieria.com',
-    'https://menteingenieria.com',
-    '*'
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1",
+    "https://3.145.58.151",
+    "https://3.145.58.151:443",
+    "http://3.145.58.151:443",
+    "http://179.2.28.24",
+    "https://179.2.28.24",
+    "http://172.26.13.221",
+    "https://172.26.13.221",
+    "https://www.menteingenieria.com",
+    "https://menteingenieria.com",
 ]
 
 ALLOWED_HOSTS = [
-    '3.145.58.151',  
-    'menteingenieria.com',
-    'www.menteingenieria.com',
-    'localhost',  
-    '172.26.13.221'
+    "3.145.58.151",
+    "menteingenieria.com",
+    "www.menteingenieria.com",
+    "localhost",
+    "172.26.13.221",
+    "127.0.0.1",
 ]
 
-ROOT_URLCONF = 'menteingenieria.urls'
+ROOT_URLCONF = "menteingenieria.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'menteingenieria.wsgi.application'
+WSGI_APPLICATION = "menteingenieria.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DBNAME'),
-        'USER': config('USERNAME'),
-        'PASSWORD': config('USERPASS'),
-        'HOST': config('HOSTNAME'),
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DBNAME"),
+        "USER": config("USERNAME"),
+        "PASSWORD": config("USERPASS"),
+        "HOST": config("HOSTNAME"),
+        "PORT": "3306",
     }
 }
 
@@ -105,92 +105,91 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-cl'
+LANGUAGE_CODE = "es-cl"
 
-TIME_ZONE = 'America/Santiago'
+TIME_ZONE = "America/Santiago"
 
 USE_I18N = True
 
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://localhost:4173',
-    'http://127.0.0.1',
-    'https://3.145.58.151',
-    'http://179.2.28.24',
-    'https://179.2.28.24',
-    'http://172.26.13.221',
-    'https://172.26.13.221',
-    'https://www.menteingenieria.com',
-    'https://menteingenieria.com',
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1",
+    "https://3.145.58.151",
+    "http://179.2.28.24",
+    "https://179.2.28.24",
+    "http://172.26.13.221",
+    "https://172.26.13.221",
+    "https://www.menteingenieria.com",
+    "https://menteingenieria.com",
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.me.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mail.me.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('MAIL_USER')
-EMAIL_HOST_PASSWORD = config('MAIL_PASS')
+EMAIL_HOST_USER = config("MAIL_USER")
+EMAIL_HOST_PASSWORD = config("MAIL_PASS")
 
 
 # Configuración para S3
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_REGION') 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_BUCKET_NAME")
+AWS_S3_REGION_NAME = config("AWS_REGION")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
+    "CacheControl": "max-age=86400",
 }
 
 # Directorio donde se guardarán temporalmente los archivos estáticos antes de subirlos a S3
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Directorio donde se guardarán temporalmente los archivos estáticos antes de subirlos a S3
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Almacenar archivos estáticos en S3
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'  
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
 # Almacenar archivos multimedia en S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 
 ## SECURITY
 SECURE_HSTS_SECONDS = 3600
-SECURE_SSL_REDIRECT = not config('DEBUG', default=False, cast=bool)
+SECURE_SSL_REDIRECT = not config("DEBUG", default=False, cast=bool)
 SECURE_HSTS_INCLUDE_SUBDOMAINS = SECURE_SSL_REDIRECT
 SESSION_COOKIE_SECURE = SECURE_SSL_REDIRECT
 SECURE_HSTS_PRELOAD = SECURE_SSL_REDIRECT
--- backend/menteingenieria/urls.py
+++ backend/menteingenieria/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -21,8 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('contacto.urls')),
-    path('api/blog/', include('blog.urls')),
-    path('api/openai_request/', include('openai_requests.urls')),
+    path("f0a46dd2d8b581227e01653297436068/", admin.site.urls),
+    path("api/", include("contacto.urls")),
+    path("api/blog/", include("blog.urls")),
+    path("api/openai_request/", include("openai_requests.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
